642a0a28efb9caaa4140a4e95f9aff5a8f8be094
gtkmshirai <t_shirai@jsk.t.u-tokyo.ac.jp>
2017-12-07 20:29:44 +0900

    modified stabilizer on air detection time range

--- rtc/Stabilizer/Stabilizer.cpp
+++ rtc/Stabilizer/Stabilizer.cpp
@@ -395,7 +395,7 @@ RTC::ReturnCode_t Stabilizer::onInitialize()
   limb_stretch_avoidance_time_const = 1.5;
   limb_stretch_avoidance_vlimit[0] = -100 * 1e-3 * dt; // lower limit
   limb_stretch_avoidance_vlimit[1] = 50 * 1e-3 * dt; // upper limit
-  detection_count_to_air = static_cast<int>(0.0 / dt);
+  detection_count_to_air = static_cast<int>(1.0 / dt);
 
   // parameters for RUNST
   double ke = 0, tc = 0;
